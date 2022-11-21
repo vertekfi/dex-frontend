1f79f071cca9b33c048163ef05813eed731c30fd
0xBriz <chaindevone@gmail.com>
2022-11-21 15:51:08 -0500

    starting bsc network config

--- lib/config/network-config.ts
+++ lib/config/network-config.ts
@@ -1,10 +1,12 @@
 import { NetworkConfig } from '~/lib/config/network-config-type';
 import { fantomNetworkConfig } from '~/lib/config/fantom';
 import { optimismNetworkConfig } from '~/lib/config/optimism';
+import { bscNetworkConfig } from './bsc';
 
 const AllNetworkConfigs: { [chainId: string]: NetworkConfig } = {
   '250': fantomNetworkConfig,
   '10': optimismNetworkConfig,
+  '56': bscNetworkConfig,
 };
 
 export const networkConfig = AllNetworkConfigs[process.env.NEXT_PUBLIC_CHAIN_ID || '250'];
@@ -21,5 +23,11 @@ export const networkList = [
     chainId: optimismNetworkConfig.chainId,
     url: 'https://op.beets.fi',
     iconUrl: optimismNetworkConfig.eth.iconUrl,
+  },
+  {
+    name: bscNetworkConfig.networkShortName,
+    chainId: bscNetworkConfig.chainId,
+    url: 'https://beets.fi',
+    iconUrl: bscNetworkConfig.eth.iconUrl,
   },
 ];
