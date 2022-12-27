4e2e994d5b1c703f585dcab7e0e1eaa0304ab692
0xBriz <chaindevone@gmail.com>
2022-12-27 10:46:17 -0500

    formatting

--- modules/voting/components/GaugeActionCard.tsx
+++ modules/voting/components/GaugeActionCard.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, GridItem, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 
-
 export interface GaugeActionCardProps {
   heading: string;
 }
@@ -9,33 +8,29 @@ export interface GaugeActionCardProps {
 export function GaugeActionCard(props: GaugeActionCardProps) {
   return (
     <GridItem
-          bg="vertek.slatepurple.900"
-          boxShadow=" 0px 0px 5px 0.5px #ECA833, 0px 5px 10px 2px #000"
-          borderRadius="25px"
-          maxW="550px"
-          color="white"
-        >
-          <Flex direction="column" justify="left" padding="8" >
-            <Text fontSize="1.2rem" paddingX="0" fontWeight="bold" >  
-            {props.heading}
-            </Text>
+      bg="vertek.slatepurple.900"
+      boxShadow=" 0px 0px 5px 0.5px #ECA833, 0px 5px 10px 2px #000"
+      borderRadius="25px"
+      maxW="550px"
+      color="white"
+    >
+      <Flex direction="column" justify="left" padding="8">
+        <Text fontSize="1.2rem" paddingX="0" fontWeight="bold">
+          {props.heading}
+        </Text>
 
-            <Flex direction="row" justify="space-between" mt="6" alignItems="center" >
-                    <Text fontSize="1.5rem" paddingX="0">  
-                        $0.00 
-                    </Text>
-                    <Text>
-                      Button
-                    </Text>
-            </Flex>
-            <Text fontSize="1rem" paddingX="0" marginTop="2">  
-                0
-            </Text>
+        <Flex direction="row" justify="space-between" mt="6" alignItems="center">
+          <Text fontSize="1.5rem" paddingX="0">
+            $0.00
+          </Text>
+          <Text>Button</Text>
+        </Flex>
+        <Text fontSize="1rem" paddingX="0" marginTop="2">
+          0
+        </Text>
+      </Flex>
 
-             
-          </Flex>
-      
-{/* Need one card for the pool values and another for the date stuff */}
+      {/* Need one card for the pool values and another for the date stuff */}
     </GridItem>
   );
 }
