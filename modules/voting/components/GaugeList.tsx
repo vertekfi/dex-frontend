ddeb0e44f964864c38fdf3fae1dc98bed3027004
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2022-12-28 14:38:48 -0500

    dummy error

--- modules/voting/components/GaugeList.tsx
+++ modules/voting/components/GaugeList.tsx
@@ -10,70 +10,6 @@ import { scale } from '~/lib/util/big-number.utils';
 import { fNum2, FNumFormats } from '~/lib/util/useNumber';
 import { useUserAccount } from '~/lib/user/useUserAccount';
 
-const { isConnected } = useUserAccount();
-
-const columns = [
-  // {
-  //   name: 'Chain',
-  //   id: 'chain',
-  //   accessor: '',
-  //   Header: 'chainColumnHeader',
-  //   Cell: 'networkColumnCell',
-  //   width: 80,
-  //   noGrow: true
-  // },
-  {
-    name: 'Icons',
-    id: 'icons',
-    accessor: 'uri',
-    Header: 'iconColumnHeader',
-    Cell: 'iconColumnCell',
-    width: 125,
-    noGrow: true
-  },
-  {
-    name: 'Composition',
-    id: 'poolComposition',
-    accessor: 'id',
-    Cell: 'poolCompositionCell',
-    width: 350
-  },
-  {
-    name: 'Next period votes',
-    accessor: 'id',
-    align: 'right',
-    id: 'nextPeriodVotes',
-    Cell: 'nextPeriodVotesCell',
-    sortKey: (gauge: { votesNextPeriod: string }) => Number(gauge.votesNextPeriod) as number,   
-    width: 150,
-    cellClassName: 'font-numeric'
-  },
-  {
-    name: 'My votes',
-    accessor(gauge: any) {
-      const normalizedVotes = scale(gauge.userVotes.toString(), -4);
-      return fNum2(normalizedVotes.toString(), {
-        style: 'percent',
-        maximumFractionDigits: 2
-      });
-    },
-    align: 'right',
-    id: 'myVotes',
-    sortKey: (gauge: any) => Number(gauge.userVotes), 
-    width: 150,
-    cellClassName: 'font-numeric',
-    hidden: !isConnected ? true : false
-  },
-  {
-    name: 'Vote',
-    id: 'vote',
-    accessor: 'id',
-    align: 'right',
-    Cell: 'voteColumnCell',
-    width: 100,
-    hidden: !isConnected ? true : false
-  }
-];
 interface GaugeListProps {
   votingGauges: VotingGauge[];
   expiredGauges: string[];
@@ -83,7 +19,70 @@ interface GaugeListProps {
 
 export function GaugeList(props: { gaugeInfo: GaugeListProps | null } | null) {
   // do the list view like thing like bal/aeq
-
+  // we out heeeeere
+  const { isConnected } = useUserAccount();
+  const columns = [
+    // {
+    //   name: 'Chain',
+    //   id: 'chain',
+    //   accessor: '',
+    //   Header: 'chainColumnHeader',
+    //   Cell: 'networkColumnCell',
+    //   width: 80,
+    //   noGrow: true
+    // },
+    {
+      name: 'Icons',
+      id: 'icons',
+      accessor: 'uri',
+      Header: 'iconColumnHeader',
+      Cell: 'iconColumnCell',
+      width: 125,
+      noGrow: true
+    },
+    {
+      name: 'Composition',
+      id: 'poolComposition',
+      accessor: 'id',
+      Cell: 'poolCompositionCell',
+      width: 350
+    },
+    {
+      name: 'Next period votes',
+      accessor: 'id',
+      align: 'right',
+      id: 'nextPeriodVotes',
+      Cell: 'nextPeriodVotesCell',
+      sortKey: (gauge: { votesNextPeriod: string }) => Number(gauge.votesNextPeriod) as number,   
+      width: 150,
+      cellClassName: 'font-numeric'
+    },
+    {
+      name: 'My votes',
+      accessor(gauge: any) {
+        const normalizedVotes = scale(gauge.userVotes.toString(), -4);
+        return fNum2(normalizedVotes.toString(), {
+          style: 'percent',
+          maximumFractionDigits: 2
+        });
+      },
+      align: 'right',
+      id: 'myVotes',
+      sortKey: (gauge: any) => Number(gauge.userVotes), 
+      width: 150,
+      cellClassName: 'font-numeric',
+      hidden: !isConnected ? true : false
+    },
+    {
+      name: 'Vote',
+      id: 'vote',
+      accessor: 'id',
+      align: 'right',
+      Cell: 'voteColumnCell',
+      width: 100,
+      hidden: !isConnected ? true : false
+    }
+  ];
   function redirectToPool(gauge: VotingGaugeWithVotes) {
     window.location.href = poolURLFor(gauge.pool.id, gauge.network);
   }
