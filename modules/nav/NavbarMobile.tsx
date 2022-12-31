71536899caf51f76b30f367e45f26ec59512469e
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2022-12-31 08:59:46 -0500

    navbar mobile reconstructed, with modal.

--- modules/nav/NavbarMobile.tsx
+++ modules/nav/NavbarMobile.tsx
@@ -5,6 +5,7 @@ import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
 import { IconButton, Collapse } from '@chakra-ui/react';
 import { ChevronUpIcon } from '@chakra-ui/icons'; 
 import { useState } from 'react';
+import { Modal, ModalContent, ModalBody, ModalFooter } from '@chakra-ui/react';
 
 export function NavbarMobile() {
   const router = useRouter();
@@ -36,22 +37,45 @@ export function NavbarMobile() {
           px="4"
         />
         <NavbarLink href={'/swap'} selected={router.asPath === '/swap'} text="Swap" mr="1" px="4" />
-
-        {networkConfig.stakeUrl && (
+        <NavbarLink href={'/staking'} selected={router.asPath === '/staking'} text="Stake" mr="1" px="4" />
+        
+        <IconButton
+        mr="1" px="4"
+        color='vertek.slatepurple.900'
+        bgColor="vertek.neonpurple.500"
+        aria-label='Click for more'
+        icon={<ChevronUpIcon />}
+        onClick={() => setIsOpen(!isOpen)}
+        />
+        <Box display="flex" padding="8px" width="100%">
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} >
+          <ModalContent  
+          width="100vw"
+          boxShadow=" 0px 0px 5px 0.5px #ECA833, 0px 5px 10px 2px #000"
+          borderRadius="25px"
+          bgGradient='linear(90deg, vertek.slatepurple.900 0%, vertek.slatepurple.700 100% )'
+          position="absolute"
+          bottom="5%"         
+          >
+          <ModalBody mt="4" alignItems="flex-start" flexDirection="column" display="flex">
+          <NavbarLink href={'/voting'} selected={router.asPath === '/voting'} text="Vote" mr="1" px="4" />
+          <NavbarLink href={'/claim'} selected={router.asPath === '/claim'} text="Claim" mr="1" px="4" />
+          </ModalBody>
+          <ModalFooter>
+            <Button variant="vertekconnect25" mr={3} onClick={() => setIsOpen(false)}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      </Box>
+        {/* {networkConfig.stakeUrl && (
           <NavbarLink href={networkConfig.stakeUrl} text="Stake" mr="1" px="4" />
-        )}
-        {networkConfig.launchUrl && (
+        )} */}
+        {/* {networkConfig.launchUrl && (
           <NavbarLink href={networkConfig.launchUrl} text="Launch" px="4" />
-        )}
-         {networkConfig.launchUrl && (
-          <IconButton
-          color='vertek.slatepurple.900'
-          bgColor="vertek.neonpurple.500"
-          aria-label='Click for more'
-          icon={<ChevronUpIcon />}
-          onClick={() => setIsOpen(!isOpen)}
-          />
-         )}
+        )} */}
+        
          
       </Flex>
        
