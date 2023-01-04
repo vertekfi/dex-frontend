d0c41bf1ad940a6cf1b8587f3ae8ff62ea1ba8f6
0xBriz <chaindevone@gmail.com>
2023-01-04 11:29:46 -0500

    formatting

--- components/batch-swap/BatchSwapList.tsx
+++ components/batch-swap/BatchSwapList.tsx
@@ -7,53 +7,53 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { replaceEthWithWeth } from '~/lib/services/token/token-util';
 
 interface Props {
-    tokenIn: string;
-    tokenOut: string;
+  tokenIn: string;
+  tokenOut: string;
 }
 
 export function BatchSwapList({ tokenIn, tokenOut }: Props) {
-    const { priceFor, getToken } = useGetTokens();
-    const { data, startPolling } = useGetPoolBatchSwapsQuery({
-        variables: {
-            first: 5,
-            skip: 0,
-            where: {
-                tokenInIn: [replaceEthWithWeth(tokenIn)],
-                tokenOutIn: [replaceEthWithWeth(tokenOut)],
-            },
-        },
-        notifyOnNetworkStatusChange: true,
-    });
+  const { priceFor, getToken } = useGetTokens();
+  const { data, startPolling } = useGetPoolBatchSwapsQuery({
+    variables: {
+      first: 5,
+      skip: 0,
+      where: {
+        tokenInIn: [replaceEthWithWeth(tokenIn)],
+        tokenOutIn: [replaceEthWithWeth(tokenOut)],
+      },
+    },
+    notifyOnNetworkStatusChange: true,
+  });
 
-    useEffect(() => {
-        startPolling(15000);
-    });
+  useEffect(() => {
+    startPolling(15000);
+  });
 
-    const batchSwaps = data?.batchSwaps || [];
+  const batchSwaps = data?.batchSwaps || [];
 
-    return (
-        <Box  padding="2">
-            {batchSwaps.map((batchSwap) => {
-                return (
-                    <AnimatePresence key={batchSwap.id}>
-                        <motion.div
-                            layout
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            layoutId={batchSwap.id}
-                        >
-                            <BatchSwapListItem
-                                batchSwap={batchSwap}
-                                tokenIn={getToken(batchSwap.tokenIn)}
-                                tokenOut={getToken(batchSwap.tokenOut)}
-                                tokenInPrice={priceFor(tokenIn)}
-                                tokenOutPrice={priceFor(tokenOut)}
-                            />
-                        </motion.div>
-                    </AnimatePresence>
-                );
-            })}
-        </Box>
-    );
+  return (
+    <Box padding="2">
+      {batchSwaps.map((batchSwap) => {
+        return (
+          <AnimatePresence key={batchSwap.id}>
+            <motion.div
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              layoutId={batchSwap.id}
+            >
+              <BatchSwapListItem
+                batchSwap={batchSwap}
+                tokenIn={getToken(batchSwap.tokenIn)}
+                tokenOut={getToken(batchSwap.tokenOut)}
+                tokenInPrice={priceFor(tokenIn)}
+                tokenOutPrice={priceFor(tokenOut)}
+              />
+            </motion.div>
+          </AnimatePresence>
+        );
+      })}
+    </Box>
+  );
 }
