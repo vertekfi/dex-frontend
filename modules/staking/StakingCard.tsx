c99bbed5b000f014f7d1a30d972cfaed2cc121c7
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-06 12:11:35 -0500

    adding vrtk logo and formatting

--- modules/staking/StakingCard.tsx
+++ modules/staking/StakingCard.tsx
@@ -1,8 +1,10 @@
 import { GridItem, Flex, Text, Box } from '@chakra-ui/react';
 import { StakingCardGuts } from './StakingCardGuts';
 import { Image } from '@chakra-ui/react';
+import NextImage from 'next/image';
 import { StakingAccordion } from './StakingAccordion';
 import { RewardPool } from '~/apollo/generated/graphql-codegen-generated';
+import Vertek from '~/assets/svg/vertektransparent.svg';
 
 export function StakingCard(props: { pool: RewardPool | null }) {
 const pool = props.pool;
@@ -16,15 +18,23 @@ return (
         maxW="550px"
         color="white"
     >
-      <Flex direction="row" justify="left" padding="2" ml="2" borderColor="box.500" borderBottomWidth="2px" alignItems="center">
-          <Box marginRight="2">
-              <Text fontSize="1rem">
-                Stake VRTK - Earn
-              </Text>
-          </Box>
-          <Box>
-              <Image src={pool.rewardToken.logoURI} alt={`Logo for ${pool.rewardToken.name}`} />
-          </Box>
+      <Flex direction="row" justify="center" fontWeight="bold" padding="2" ml="2" borderColor="box.500" borderBottomWidth="2px" alignItems="center">
+        <Box marginRight="2">
+          <Text fontSize="1rem">
+            Stake 
+          </Text>
+        </Box>
+        <Box>
+          <NextImage objectFit="contain" src={Vertek} alt={`Logo for deposit token}`} />
+        </Box>
+        <Box marginRight="2" marginLeft="12">
+          <Text fontSize="1rem">
+            Earn
+          </Text>
+        </Box>
+        <Box>
+          <Image src={pool.rewardToken.logoURI} alt={`Logo for ${pool.rewardToken.name}`} />
+        </Box>
       </Flex>
       <StakingCardGuts pool={pool} />
       <StakingAccordion pool={pool} />
