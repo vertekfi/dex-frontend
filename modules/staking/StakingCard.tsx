0450141e5d9130e4b7ab77da1d25f0a7042d83a6
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-06 11:56:23 -0500

    Refactor, Symbol fix

--- modules/staking/StakingCard.tsx
+++ modules/staking/StakingCard.tsx
@@ -1,43 +1,34 @@
-import { GridItem, Flex, Text } from '@chakra-ui/react';
+import { GridItem, Flex, Text, Box } from '@chakra-ui/react';
 import { StakingCardGuts } from './StakingCardGuts';
-import Image from 'next/image';
-import placeholder from '../../assets/images/fantom-logo.png';
+import { Image } from '@chakra-ui/react';
 import { StakingAccordion } from './StakingAccordion';
 import { RewardPool } from '~/apollo/generated/graphql-codegen-generated';
 
 export function StakingCard(props: { pool: RewardPool | null }) {
-  const pool = props.pool;
+const pool = props.pool;
 
-  return (
-    <>
-      {pool && (
-        <GridItem
-          bgGradient="vertek.slatepurple.900"
-          boxShadow="0 0 10px #5BC0F8, 0 0 20px #4A4AF6"
-          borderRadius="18px"
-          maxW="550px"
-          color="white"
-        >
-          <Flex justify="left" padding="4" borderColor="box.500" borderBottomWidth="2px">
-            <div
-              style={{
-                position: 'relative',
-                width: '20%',
-                paddingBottom: '10%',
-                marginRight: '5%',
-              }}
-            >
-              <Image src={placeholder} layout="fill" objectFit="contain" />
-            </div>
-            <Flex direction="column">
-              {/* <Text fontSize="1.5rem"> DOGE </Text> */}
-              <Text fontSize="1rem"> Stake VRTK - Earn {pool.rewardToken.symbol} </Text>
-            </Flex>
-          </Flex>
-          <StakingCardGuts pool={pool} />
-          <StakingAccordion pool={pool} />
-        </GridItem>
-      )}
-    </>
-  );
+return (
+  pool ? (
+    <GridItem
+        bgGradient="vertek.slatepurple.900"
+        boxShadow="0 0 10px #5BC0F8, 0 0 20px #4A4AF6"
+        borderRadius="18px"
+        maxW="550px"
+        color="white"
+    >
+      <Flex direction="row" justify="left" padding="2" ml="2" borderColor="box.500" borderBottomWidth="2px" alignItems="center">
+          <Box marginRight="2">
+              <Text fontSize="1rem">
+                Stake VRTK - Earn
+              </Text>
+          </Box>
+          <Box>
+              <Image src={pool.rewardToken.logoURI} alt={`Logo for ${pool.rewardToken.name}`} />
+          </Box>
+      </Flex>
+      <StakingCardGuts pool={pool} />
+      <StakingAccordion pool={pool} />
+    </GridItem>
+  ) : null
+);
 }
