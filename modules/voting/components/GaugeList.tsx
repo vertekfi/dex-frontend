05f8d363675a1f7f54f857847c65d307350c5397
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-12 16:41:50 -0500

    skeleton

--- modules/voting/components/GaugeList.tsx
+++ modules/voting/components/GaugeList.tsx
@@ -1,5 +1,5 @@
 import { isSameAddress } from '@balancer-labs/sdk';
-import { Box, Text, Flex, Grid, GridItem } from '@chakra-ui/react';
+import { Box, Text, Flex, Grid, Skeleton } from '@chakra-ui/react';
 import PoolListSortLink from '~/modules/pools/components/PoolListSortLink';
 import { VotingGauge, VotingGaugeWithVotes } from '~/lib/services/staking/types';
 import { poolURLFor } from '~/modules/pool/lib/pool-utils';
@@ -10,12 +10,16 @@ import { fNum2, FNumFormats } from '~/lib/util/useNumber';
 import { useUserAccount } from '~/lib/user/useUserAccount';
 import { GaugeListTableHeader } from './GaugeListTableHeader';
 import { GaugeListItem } from './GaugeListItem';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 interface GaugeListProps {
   votingGauges: VotingGaugeWithVotes[];
 }
+
 export function GaugeList(props: GaugeListProps | null) {
   const { isConnected } = useUserAccount();
+  const [isLoading, setIsLoading] = useState(true);
   function redirectToPool(gauge: VotingGaugeWithVotes) {
     window.location.href = poolURLFor(gauge.pool.id, gauge.network);
   }
@@ -27,21 +31,23 @@ export function GaugeList(props: GaugeListProps | null) {
   return (
     <PoolListProvider>
       <UserTokenBalancesProvider>
-        <Box 
-        mt="3rem" 
-        boxShadow={{base: "none", lg:"0 0 10px #5BC0F8, 0 0 20px #4A4AF6" }} 
-        mb="6rem" 
-        borderRadius="16px" 
-        flexDirection="column" 
-        display="flex">
-          <GaugeListTableHeader />
-          <Box>
-            {props?.votingGauges.map((gauge) => {
-              return <GaugeListItem key={gauge.address} gauge={gauge} />;
-            })}
+        <Skeleton isLoaded={!isLoading}>
+          <Box 
+          mt="3rem" 
+          boxShadow={{base: "none", lg:"0 0 10px #5BC0F8, 0 0 20px #4A4AF6" }} 
+          mb="6rem" 
+          borderRadius="16px" 
+          flexDirection="column" 
+          display="flex">
+            <GaugeListTableHeader />
+            <Box>
+              {props?.votingGauges.map((gauge) => {
+                return <GaugeListItem key={gauge.address} gauge={gauge} />;
+              })}
+            </Box>
           </Box>
-        </Box>
+        </Skeleton>
       </UserTokenBalancesProvider>
     </PoolListProvider>
   );
-}
+            }  
