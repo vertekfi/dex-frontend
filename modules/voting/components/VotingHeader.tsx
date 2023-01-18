96b643fa4a15019be769177f8c41a2afc254a7fd
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-18 09:53:01 -0500

    voting header and lock form update

--- modules/voting/components/VotingHeader.tsx
+++ modules/voting/components/VotingHeader.tsx
@@ -148,113 +148,112 @@ export function VotingHeader() {
   }
 
   return (
-    <UserDataProvider>
-      <Grid
-        templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(4, 1fr)' }}
-        paddingX={{ base: '0', md: '6', lg: '4' }}
-        paddingY={2}
-        gap="4"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <VotingCard>
-          <Grid paddingX="2" paddingY="2">
-            <GridItem mt="2">
+<UserDataProvider>
+  <Grid
+    templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(4, 1fr)' }}
+    paddingX={{ base: '0', md: '6', lg: '4' }}
+    paddingY={2}
+    gap="4"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <VotingCard>
+      <Grid paddingX="2" paddingY="2">
+          <GridItem mt="2">
               <VotingCardHeader>My 80VRTK-20BNB</VotingCardHeader>
-            </GridItem>
-            <GridItem mt="3">
-              <SubText>{userPoolBalance.usdValue}</SubText>
-            </GridItem>
-            <GridItem mt="-1">
-              <ValueText>{userPoolBalance.balance}</ValueText>
-            </GridItem>
-            <GridItem mt={{ base: '3', lg: '6' }}>
-              <Box display="flex" justifyContent="center">
-                <Button 
-                as="a"
-                href="/pool/0xc107b351b787e64c0a59a1f44cb393704da07d3f000200000000000000000006"
-                target=""
-                variant="stayblack" width={{ base: '50%', lg: '75%' }}
-                >
-                  Get VRTK-BNB
-                </Button>
+          </GridItem>
+          <GridItem mt="3">
+            <SubText>{userPoolBalance.usdValue}</SubText>
+          </GridItem>
+          <GridItem mt="-1">
+            <ValueText>{userPoolBalance.balance}</ValueText>
+          </GridItem>
+          <GridItem mt={{ base: '3', lg: '6' }}>
+            <Box display="flex" justifyContent="center">
+              <Button 
+              as="a"
+              href="/pool/0xc107b351b787e64c0a59a1f44cb393704da07d3f000200000000000000000006"
+              target=""
+              variant="stayblack" width={{ base: '50%', lg: '75%' }}
+              >
+                Get VRTK-BNB
+              </Button>
 
-                {/* will need to update this link when the real pool is live!  */}
-              </Box>
-            </GridItem>
-          </Grid>
-        </VotingCard>
-
-        <VotingCard>
-          <Grid paddingX="2" paddingY="2">
-            <GridItem mt="2">
-              <VotingCardHeader>My locked 80VRTK-20BNB</VotingCardHeader>
-            </GridItem>
-            <GridItem mt="3">
-              <SubText>$0.00</SubText>
-            </GridItem>
-            <GridItem mt="-1">
-              <ValueText>{userLockInfo?.lockedAmount}</ValueText>
-            </GridItem>
-            <GridItem mt={{ base: "3", lg: "6" }}>
-        <Box display="flex" justifyContent="center">
-          <Button
-            variant="stayblack"
-            width={{ base: "50%", lg: "75%" }}
-            onClick={handleOpenModal}
-          >
-            Lock VRTK-BNB
-          </Button>
-        </Box>
-      </GridItem>
-      {isModalOpen && (
-        <LockForm
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-      )}
-          </Grid>
-        </VotingCard>
-
-        <VotingCard>
-          <Grid paddingX="2" paddingY="2">
-            <GridItem mt="2">
-              <VotingCardHeader>Locked until</VotingCardHeader>
-            </GridItem>
-            <GridItem mt="3">
-              <SubText>{lockInfoDisplay.lockedUntilDate}</SubText>
-            </GridItem>
-            <GridItem mt="-1">
-              <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
-                <ValueText>{lockInfoDisplay.lockedUntilDays} days</ValueText>
-              </Box>
-            </GridItem>
-
-            <GridItem mt={{ base: '3', lg: '6' }}>
-              <Box display="flex" justifyContent="center">
-                <Button variant="stayblack" width={{ base: '50%', lg: '75%' }}>
-                  Update My Lock
-                  <LockIcon ml="2" color="vertek.slate.200" />
-                </Button>
-              </Box>
-            </GridItem>
-          </Grid>
-        </VotingCard>
-
-        <VotingCard>
-          <Grid paddingX="2" paddingY="2">
-            <GridItem mt="2">
-              <VotingCardHeader>My veVRTK</VotingCardHeader>
-            </GridItem>
-            <GridItem mt="3">
-              <SubText>{lockInfoDisplay.veBalance}</SubText>
-            </GridItem>
-            <GridItem mt="-1">
-              <ValueText>{lockInfoDisplay.percentOwned}</ValueText>
-            </GridItem>
-          </Grid>
-        </VotingCard>
+              {/* will need to update this link when the real pool is live!  */}
+            </Box>
+          </GridItem>
       </Grid>
-    </UserDataProvider>
+    </VotingCard>
+
+    <VotingCard>
+      <Grid paddingX="2" paddingY="2">
+        <GridItem mt="2">
+          <VotingCardHeader>My locked 80VRTK-20BNB</VotingCardHeader>
+        </GridItem>
+        <GridItem mt="3">
+          <SubText>$0.00</SubText>
+        </GridItem>
+        <GridItem mt="-1">
+          <ValueText>{userLockInfo?.lockedAmount}</ValueText>
+        </GridItem>
+        <GridItem mt={{ base: "3", lg: "6" }}>
+          <Box display="flex" justifyContent="center">
+            <Button
+              variant="stayblack"
+              width={{ base: "50%", lg: "75%" }}
+              onClick={handleOpenModal} >
+              Lock VRTK-BNB
+            </Button>
+          </Box>
+        </GridItem>
+          {isModalOpen && (
+            <LockForm
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+            />
+          )}
+      </Grid>
+    </VotingCard>
+
+    <VotingCard>
+      <Grid paddingX="2" paddingY="2">
+        <GridItem mt="2">
+          <VotingCardHeader>Locked until</VotingCardHeader>
+        </GridItem>
+        <GridItem mt="3">
+          <SubText>{lockInfoDisplay.lockedUntilDate}</SubText>
+        </GridItem>
+        <GridItem mt="-1">
+          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
+            <ValueText>{lockInfoDisplay.lockedUntilDays} days</ValueText>
+          </Box>
+        </GridItem>
+
+        <GridItem mt={{ base: '3', lg: '6' }}>
+          <Box display="flex" justifyContent="center">
+            <Button variant="stayblack" width={{ base: '50%', lg: '75%' }}>
+              Update My Lock
+              <LockIcon ml="2" color="vertek.slate.200" />
+            </Button>
+          </Box>
+        </GridItem>
+      </Grid>
+    </VotingCard>
+
+    <VotingCard>
+      <Grid paddingX="2" paddingY="2">
+        <GridItem mt="2">
+          <VotingCardHeader>My veVRTK</VotingCardHeader>
+        </GridItem>
+        <GridItem mt="3">
+          <SubText>{lockInfoDisplay.veBalance}</SubText>
+        </GridItem>
+        <GridItem mt="-1">
+          <ValueText>{lockInfoDisplay.percentOwned}</ValueText>
+        </GridItem>
+      </Grid>
+    </VotingCard>
+  </Grid>
+</UserDataProvider>
   );
 }
--- modules/voting/components/lock/LockForm.tsx
+++ modules/voting/components/lock/LockForm.tsx
@@ -1,16 +1,9 @@
-import { Modal, Text, ModalOverlay, ModalContent, GridItem, ModalCloseButton,
-Grid, Box, Button, Flex } from "@chakra-ui/react";
+import { Modal, Text, ModalOverlay, ModalContent, GridItem, ModalCloseButton, Grid, Box, Button, Flex } from "@chakra-ui/react";
 import { BeetsModalBody, BeetsModalContent, BeetsModalHeader, BeetsModalHeadline } from '~/components/modal/BeetsModal';
 import { useState } from 'react';
 import { FormControl, FormLabel, Input } from '@chakra-ui/react';
 import { Calendar } from "react-feather";
-import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
-} from '@chakra-ui/react';
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, } from '@chakra-ui/react';
 
 interface Props {
 isOpen: boolean;
@@ -22,20 +15,30 @@ return (
     <Modal 
     isOpen={props.isOpen} 
     onClose={props.onClose} 
-    size="lg">
+    size="xl"
+    >
     <ModalOverlay bgColor="vertek.slate.900"/> 
-    <ModalContent minW='90vw' maxW='90vw' bg="vertek.slate.900" paddingY="2rem" borderRadius="12px" >
+    <ModalContent 
+    minW='90vw' maxW='90vw' 
+    bg="vertek.slatepurple.900" 
+    paddingY="2rem" borderRadius="12px" 
+    marginTop="1.5rem">
         <ModalCloseButton />
         <BeetsModalHeader>
-            <BeetsModalHeadline textAlign="center" fontSize="1.5rem" color="white" mb="12px" >
-                    Lock VRTK-WBNB
+            <BeetsModalHeadline textAlign="center" fontSize="1.5rem" color="white" mb="12px" mt="-1rem" >
+                Lock VRTK-WBNB
             </BeetsModalHeadline>
         </BeetsModalHeader>
     <BeetsModalBody bgColor="transparent" p="4" textAlign="center" fontSize="1.2rem">
         <Grid pl="4" pr="4" templateColumns={{ base: '1fr 200px 1fr', md: '1fr 400px 1fr' }}>
-            <GridItem m="2">
-                <Box padding="2" borderRadius="12px" mb="6" bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)">
-                    <Text fontSize="1rem" fontWeight="bold" align="start">
+        <GridItem 
+            boxShadow="0 0 5px #fff"
+            m="2" 
+            bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)" 
+            borderRadius="12px">
+                <Box padding="2" borderRadius="12px" mb="6" >
+                <Text align="left" padding="1" mb="4" fontWeight="bold" color="white" fontSize="1.2rem">
+
                         Lockable tokens in my wallet
                     </Text>
                     <Flex align="center" mt="6">
@@ -74,12 +77,18 @@ return (
                             </AccordionPanel>
                             </AccordionItem>
                         </Accordion>
-                    {/* */}
+
                 </Box>
             </GridItem>
-            <GridItem m="2">
+            <GridItem 
+            boxShadow="0 0 5px #fff"
+            m="2" 
+            bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)" 
+            borderRadius="12px">
             <Box padding="2" borderRadius="12px" mb="6" bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)">
-                <Text align="left" padding="1" mb="4" fontWeight="bold" color="white" fontSize="1.2rem" textDecoration="uppercase" >Lock to get veVRTK</Text>
+                <Text align="left" padding="1" mb="4" fontWeight="bold" color="white" fontSize="1.2rem">
+                    Lock to get veVRTK
+                </Text>
                 <Text 
                 align="left" 
                 paddingX="3" mb="0" 
@@ -177,9 +186,15 @@ return (
                     </Button>
                 </Box>
             </GridItem>
-            <GridItem m="2">
+            <GridItem 
+            boxShadow="0 0 5px #fff"
+            m="2" 
+            bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)" 
+            borderRadius="12px">
             <Box padding="2" borderRadius="12px" mb="6" bgGradient="linear-gradient(90deg, #302B84 0%, #362BA8 80%, #4132D0 100%)">
-                <Text fontSize="1rem" fontWeight="bold" align="start">My veVRTK</Text>
+            <Text align="left" padding="1" mb="4" fontWeight="bold" color="white" fontSize="1.2rem">
+                    My veVRTK
+                    </Text>
                 <Box mt="20" >
                     <Text fontSize="1.2rem" fontWeight="bold" align="center">0 veVRTK</Text>
                 </Box>
