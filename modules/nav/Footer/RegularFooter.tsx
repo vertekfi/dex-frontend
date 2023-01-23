92155c66e7ffdc2e3d5b0be94681468bdd17a4ae
TheHomelessCoder <unknown@unkown.com>
2023-01-23 14:57:43 -0600

    adding in footer socials opacity on hover

--- modules/nav/Footer/RegularFooter.tsx
+++ modules/nav/Footer/RegularFooter.tsx
@@ -10,126 +10,133 @@ import { networkConfig } from '~/lib/config/network-config';
 import { BeetsBalLogo } from '~/assets/logo/BeetsBalLogo';
 import { BeetsLogo } from '~/assets/logo/BeetsLogo';
 import { MobileFooter } from './MobileFooter';
+import styled from '@emotion/styled';
 
 export function RegularFooter() {
-const { chainId } = useNetworkConfig();
+  const { chainId } = useNetworkConfig();
 
-return (
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
 
-<Box 
-as="footer"
-display={{ base:'none', md:'flex' }} 
-flexDirection="row" 
-minWidth="full" px={{ base: '4', xl: '4' }} 
-bgColor="vertek.slatepurple.900"   
-fontWeight="bold" 
-justifyContent="space-between"
-paddingY="2"
->
-    <Box 
-    display="flex" 
-    flexDirection="column" 
-    justifyContent="space-between" 
-    alignItems="flex-start"
-    textAlign="left" 
+  const StyledLink = styled(Link)`
+  &:hover{
+      opacity: .8;
+    }
+  `;
+
+  return (
+    <Box
+      as="footer"
+      display={{ base: 'none', md: 'flex' }}
+      flexDirection="row"
+      minWidth="full"
+      px={{ base: '4', xl: '4' }}
+      bgColor="vertek.slatepurple.900"
+      fontWeight="bold"
+      justifyContent="space-between"
+      paddingY="2"
     >
-      <FooterLink href="/pools" linkType="internal" textShadow="none">
-            Invest
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        textAlign="left"
+      >
+        <FooterLink href="/pools" linkType="internal" textShadow="none">
+          Invest
         </FooterLink>
         <FooterLink href="/swap" linkType="internal">
-            Swap
+          Swap
         </FooterLink>
         <FooterLink href="/staking" linkType="internal">
-            Stake
-            <Text as="span" fontSize="2xs">
-              {' '}
-              (BSC)
-            </Text>
+          Stake
+          <Text as="span" fontSize="2xs">
+            {' '}
+            (BSC)
+          </Text>
         </FooterLink>
         <FooterLink href="/voting" linkType="internal">
-            veVRTK
+          veVRTK
         </FooterLink>
-       
-    </Box>
-    <Box 
-    display="flex"
-    justifyContent="space-between" 
-    mb="6"
-    alignItems="center" 
-    flexDirection="column" 
-    borderBottomWidth="0px" 
-    borderColor="vertek.neonpurple.500" 
-    paddingY="2" paddingX="2" 
-    >
-      {chainId === '10' ? 
-      <BeetsBalLogo mt="1" width="150px"  /> : <BeetsLogo width="150px" mt="1" />}
-      <HStack 
-      spacing="10" 
-      mt="" 
-      display="flex" 
-      justifyContent="center" 
-      alignItems="center" 
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        mb="6"
+        alignItems="center"
+        flexDirection="column"
+        borderBottomWidth="0px"
+        borderColor="vertek.neonpurple.500"
+        paddingY="2"
+        paddingX="2"
       >
-        <Box>
-          <Link
-            href=" https://discord.gg/vertek-ames-aalto"
-            target="_blank"
-            _active={{ boxShadow: 'none' }}
-          >
-            <NextImage src={DiscordIcon} />
-          </Link>
-        </Box>
-        <Box>
-          <Link
-            href=" https://twitter.com/Vertek_Dex"
-            target="_blank"
-            _active={{ boxShadow: 'none' }}
-          >
-            <NextImage src={TwitterIcon} />
-          </Link>
-        </Box>
-        <Box>
-          <Link
-            href="https://github.com/vertekfi/vertek-lib"
-            target="_blank"
-            _active={{ boxShadow: 'none' }}
-          >
-            <NextImage src={GithubIcon} />
-          </Link>
-        </Box>
-        <Box>
-          <Link
-            href="https://medium.com/@verteklabs"
-            target="_blank"
-            _active={{ boxShadow: 'none' }}
-          >
-            <NextImage src={MediumIcon} />
-          </Link>
-        </Box>
-      </HStack>
-    </Box>
-    <Box 
-    display="flex" 
-    flexDirection="column" 
-    justifyContent="space-between" 
-    alignItems="flex-end"
-    textAlign="left" 
-    textShadow="none"
-    >
-         <FooterLink href="https://aalto-defi.notion.site/Vertek-Dex-d9958759f4424962a700bb2cf90559ea">
-            Whitepaper
+        {chainId === '10' ? (
+          <BeetsBalLogo mt="1" width="150px" />
+        ) : (
+          <BeetsLogo width="150px" mt="1" />
+        )}
+        <HStack spacing="10" mt="" display="flex" justifyContent="center" alignItems="center">
+          <Box>
+            <StyledLink
+              href=" https://discord.gg/vertek-ames-aalto"
+              target="_blank"
+              _active={{ boxShadow: 'none' }}
+            >
+              <NextImage src={DiscordIcon} />
+            </StyledLink>
+          </Box>
+          <Box>
+            <StyledLink
+              href=" https://twitter.com/Vertek_Dex"
+              target="_blank"
+              _active={{ boxShadow: 'none' }}
+            >
+              <NextImage src={TwitterIcon} />
+            </StyledLink>
+          </Box>
+          <Box>
+            <StyledLink
+              href="https://github.com/vertekfi/vertek-lib"
+              target="_blank"
+              _active={{ boxShadow: 'none' }}
+            >
+              <NextImage src={GithubIcon} />
+            </StyledLink>
+          </Box>
+          <Box>
+            <StyledLink
+              href="https://medium.com/@verteklabs"
+              target="_blank"
+              _active={{ boxShadow: 'none' }}
+            >
+              <NextImage src={MediumIcon} />
+            </StyledLink>
+          </Box>
+        </HStack>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        alignItems="flex-end"
+        textAlign="left"
+        textShadow="none"
+      >
+        <FooterLink href="https://aalto-defi.notion.site/Vertek-Dex-d9958759f4424962a700bb2cf90559ea">
+          Whitepaper
         </FooterLink>
         <FooterLink href="https://aalto-defi.notion.site/Tokenomics-199366c64eb64684b33684ffe6b6bf89">
-            Tokenomics
+          Tokenomics
         </FooterLink>
-        <FooterLink 
-              href="https://aalto-defi.notion.site/Liquidity-Pool-Types-9ee6d04b7cab4f1b8352e0db492b7672">
-            Pool Types 
+        <FooterLink href="https://aalto-defi.notion.site/Liquidity-Pool-Types-9ee6d04b7cab4f1b8352e0db492b7672">
+          Pool Types
         </FooterLink>
-       
-       
+
         <FooterLink href={networkConfig.createPoolUrl}>Create a pool</FooterLink>
+      </Box>
     </Box>
-</Box>
-);
+  );
 }
