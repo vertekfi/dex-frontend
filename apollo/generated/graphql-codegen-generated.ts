6907506915170922cdaa1326309eab45e46dd555
0xBriz <chaindevone@gmail.com>
2023-01-22 23:11:48 -0500

    gql codegen sync with backend

--- apollo/generated/graphql-codegen-generated.ts
+++ apollo/generated/graphql-codegen-generated.ts
@@ -1132,6 +1132,7 @@ export interface Query {
   blocksGetBlocksPerDay: Scalars['Int'];
   contentGetNewsItems: Array<Maybe<GqlContentNewsItem>>;
   getLiquidityGauges: Array<Maybe<LiquidityGauge>>;
+  getProtocolPoolData: Array<Maybe<Scalars['String']>>;
   getProtocolTokenList?: Maybe<Array<Maybe<Scalars['String']>>>;
   getRewardPools: Array<Maybe<RewardPool>>;
   getUserGaugeStakes: Array<Maybe<LiquidityGauge>>;
