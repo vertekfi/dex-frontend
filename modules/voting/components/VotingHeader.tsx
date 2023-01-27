950d040a48d5347ab4ca528f9a10bb843333e8d3
0xBriz <chaindevone@gmail.com>
2023-01-27 12:18:17 -0500

    gauge voting modal working now

--- modules/voting/components/VotingHeader.tsx
+++ modules/voting/components/VotingHeader.tsx
@@ -76,8 +76,6 @@ export function VotingHeader() {
       if (userLockInfo.hasExistingLock && !userLockInfo.isExpired) {
         setHasLock(true);
 
-        console.log(bnum(userLockInfo.lockedAmount).div(userLockInfo.totalSupply).toString());
-
         const percentOwned = fNum2(
           bnum(userLockInfo.lockedAmount).div(userLockInfo.totalSupply).toString(),
           {
