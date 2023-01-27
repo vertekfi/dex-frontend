8976c09e55f4c8f362494f7fa36714d4560c7455
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-27 09:43:42 -0500

    lockform

--- modules/voting/components/lock/LockForm.tsx
+++ modules/voting/components/lock/LockForm.tsx
@@ -102,8 +102,8 @@ return (
     marginTop="2rem"
     padding="4" 
     >
-    <ModalCloseButton />
-    <BeetsModalHeader  >
+  <ModalCloseButton />
+  <BeetsModalHeader  >
       <BeetsModalHeadline
         textAlign="center"
         fontSize="1.5rem"
@@ -113,97 +113,125 @@ return (
         >
         Lock VRTK-BNB
       </BeetsModalHeadline>
-    </BeetsModalHeader>
+  </BeetsModalHeader>
       
-    <BeetsModalBody bgColor="transparent" p="0" textAlign="center" fontSize="1.2rem">
-      <Grid
-        backgroundColor="transparent" 
-        pl={{ base: '2', md: '4' }}
-        pr={{ base: '2', md: '4' }}
-        templateColumns={{ base: '1fr', md: '2fr 3fr 2fr' }}
-        gap="4"
-      >
-        <GridItem
-          boxShadow={{base:'none', md:'0 12px 12px #000'}}
-          width={{base:'90%', lg:'auto'}}
-          height={{ base:'auto', lg:'50%'}}
-          m={{ base:'2', md:'2'}}
-          mt={{ base:'auto', md:'24'}}
-          backdropFilter="blur(12px)"
-          bgColor="vertek.slate.900"
-          borderRadius="16px">
-          <Box 
-              padding={{ base: '2', md: '2' }} 
-              borderRadius="12px" mb="6">
-            <Text
-              align="left"
-              padding="1"
-              mb="4"
-              fontWeight="bold"
-              color="white"
-              fontSize="1.2rem"
-            >
-                Lockable tokens in my wallet
-            </Text>
-            <Flex align="center" mt="6">
-                <Text fontSize="1rem" mr="auto">
-                  VRTK-BNB
-                </Text>
-                <Text fontSize="1rem" ml="auto">
-                {bptBalanceForPool(networkConfig.balancer.votingEscrow.lockablePoolId)} shares
-                </Text>
-            </Flex>
-            <Flex align="center" mt="1">
-                <Text fontSize="1rem" mr="auto">
-                  Vertek Governance
-                </Text>
-                <Text fontSize="1rem" ml="auto">
-                ${usdBalanceForPool(networkConfig.balancer.votingEscrow.lockablePoolId)}
-                </Text>
-            </Flex>
-            <Flex mt="8">
-                <Button
-                  variant="stayblacklock"
-                  as="a"
-                  href={'pool/' + networkConfig.balancer.votingEscrow.lockablePoolId}
-                  borderWidth="1px"
-                  width="100%"
-                  height="2.2rem"
-                  boxShadow="0 0 12px rgba(0, 0, 0, 1)" 
-                >
-                  Get VRTK-BNB
-                </Button>
-            </Flex>
-          </Box>
-          <Box
-            padding="2"
-            borderRadius="12px"
-            mb="6"
-            bgColor="vertek.slate.900"
-          >
-            <Text fontSize="1rem" mr="auto"></Text>
-              <Accordion allowToggle padding={1}>
-                <AccordionItem>
-                  <AccordionButton _expanded={{}}>
-                    <Box flex="1" textAlign="center">
-                      How to lock
-                    </Box>
-                    <AccordionIcon color="black" />
-                  </AccordionButton>
+  <BeetsModalBody bgColor="transparent" 
+    p="0" textAlign="center" fontSize="1.2rem">
+  
+    <Grid
+      backgroundColor="transparent" 
+      pl={{ base: '0', md: '4' }}
+      pr={{ base: '0', md: '4' }}
+      templateColumns={{ base: '1fr', md: '2fr 3fr 2fr' }}
+      gap="4"
+    >
+      <GridItem
+        width={{base:'90%', lg:'auto'}}
+        height={{ base:'90%', lg:'50%'}}
+        m={{ base:'0', md:'2'}}
+        mt={{ base:'2', md:'12'}}
+        bgColor="vertek.slate.900"
+        borderRadius="16px">
+              <Text
+                align="left"
+                padding="2"
+                mb="4"
+                fontWeight="bold"
+                color="white"
+                fontSize="1.2rem"
+              >
+                  Lockable tokens in my wallet
+              </Text>
+              <Box 
+                  display="flex" 
+                  justifyContent="space-between" 
+                  alignItems="space-between" 
+                  height="40%"
+                  marginX="2" mb="6"
+                  padding="2" paddingY="4"
+                  bgColor="vertek.slatepurple.900"
+                  boxShadow="2px 12px 12px 0px #000"
+                  borderRadius="12px" 
+                  flexDirection="column" 
+                  >
+                    <Flex>
+                        <Text fontSize="1rem" mr="auto">
+                          VRTK-BNB
+                        </Text>
+                        <Text fontSize="1rem" ml="auto">
+                        {bptBalanceForPool(networkConfig.balancer.votingEscrow.lockablePoolId)} shares
+                        </Text>
+                    </Flex>
+                    <Flex>
+                        <Text fontSize="1rem" mr="auto">
+                          Vertek Governance
+                        </Text>
+                        <Text fontSize="1rem" ml="auto">
+                        ${usdBalanceForPool(networkConfig.balancer.votingEscrow.lockablePoolId)}
+                        </Text>
+                    </Flex>
+              </Box>
 
-                  <AccordionPanel pb={5} bg="">
-                    <Text align="left" color="white" fontSize="1rem">
-                      1. Invest in the VRTK-BNB pool. <br />
-                      2. Lock VRTK-BNB to receive veVRTK <br />
-                      3. veVRTK holders receive boosted liquidity mining yields (up to 2.5x) and
-                      increased voting power. <br />
-                      4. This voting power can be used to choose which pool gauges get allocated liquidity
-                      mining incentives.
-                    </Text>
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
-            </Box>
+              <Flex display="flex" justifyContent="center" alignItems="center" 
+              mt="8">
+                  <Button
+                    variant="verteklight"
+                    as="a"
+                    href={'pool/' + networkConfig.balancer.votingEscrow.lockablePoolId}
+                    borderWidth="1px"
+                    width="90%"
+                    height="2.2rem"
+                    
+                  >
+                    Get VRTK-BNB
+                  </Button>
+              </Flex>
+
+              <Box
+                padding="2"
+                borderRadius="16px"
+                bgColor="vertek.slate.900"
+                boxShadow="2px 24px 12px 0px #000"
+                mb="4" mt="4" 
+              >
+                <Text fontSize="1rem" mr="auto"></Text>
+                  <Accordion allowToggle mb="4" padding={1}>
+                    <AccordionItem>
+                      <AccordionButton color="white"  _expanded={{}}>
+                      <AccordionIcon color="white" />
+                        <Box flex="1" textAlign="center">
+                          How to lock
+                        </Box>
+                        <AccordionIcon color="white" />
+                      </AccordionButton>
+
+                      <AccordionPanel
+                      height={{ base: '40vh', md:'auto' }}
+                      bg="vertek.slatepurple.900"
+                      padding="4" 
+                      marginY="4" 
+                      borderRadius="16px"
+                      boxShadow="2px 12px 12px 0px #000"
+
+                      >
+                        <Text align="left" color="white" fontSize="0.9rem" mb="0.5rem">
+                          1. Invest in the VRTK-BNB pool. 
+                        </Text>  
+                        <Text align="left" color="white" fontSize="0.9rem" mb="0.5rem">
+                          2. Lock VRTK-BNB to receive veVRTK.
+                        </Text> 
+                        <Text align="left" color="white" fontSize="0.9rem" mb="0.5rem">
+                        3. Receive boosted liquidity mining yields (up to 2.5x) and
+                          increased voting power.
+                        </Text>
+                        <Text align="left" color="white" fontSize="0.9rem" mb="0.5rem">
+                        4. Choose which pool gauges get allocated liquidity
+                          mining incentives.
+                        </Text>
+                      </AccordionPanel>
+                    </AccordionItem>
+                  </Accordion>
+                </Box>
           </GridItem>
           <GridItem
             boxShadow={{ base: 'none', md: '0 12px 12px #000' }}
