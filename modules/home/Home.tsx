5031819335537a290b5ddda13349556079dafe24
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-01-31 09:53:32 -0500

    icon intuitive

--- modules/home/Home.tsx
+++ modules/home/Home.tsx
@@ -8,6 +8,8 @@ import { useUserData } from '~/lib/user/useUserData';
 import { useGetPoolsLazyQuery } from '~/apollo/generated/graphql-codegen-generated';
 import { useEffect } from 'react';
 import { HomePoolsNew } from './components/HomePoolsNew';
+import Image from 'next/image';
+import puzzle from '~/assets/vertekicons/puzzle.svg'; 
 
 
 
@@ -35,42 +37,21 @@ export function Home() {
 
   return (
     <Box>
-      <Box display="flex" justifyContent="flex-end">
-        {/* <BeetsLogo /> */}
-      </Box>
       <HomeHero />
-      {/* <Grid
-          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(4, 1fr)" }}
-          columnGap={{ base: "0", lg: "8" }}
-          rowGap="12"
-          mt="12"
-          borderBottomWidth={2}
-          borderBottomColor="gray.100"
-          pb="24"
-          >
-      {featuredPoolGroups.map((group) =>
-      group.items.map((item) => {
-      switch (item.__typename) {
-          case "GqlPoolMinimal":
-            return <PoolCard pool={item} key={item.id} />;
-          case "GqlFeaturePoolGroupItemExternalLink":
-          // Render something for the external link item
-      }
-    })
-  )}
-          <GridItem>
-            <HomeNews />
-          </GridItem>
-      </Grid> */}
       <HomePoolsNew />
-      <Text mb="0" mt="12" fontSize={{ base: '2rem', md: '3rem'}}  
+      <Text mb="4" mt="12" fontSize={{ base: '2rem', md: '3rem'}}  
       lineHeight={{ base: '1.1', md: 'auto' }} 
       textAlign={{ base: 'left', md:'justify' }} className="vertektitle">
         The pinnacle of DeFi technological evolution. <br /> 
       </Text>
-      <Text mt="1" fontSize={{ base: '1.3rem', md:"2rem"}}  mb="8" textAlign={{ base: 'left', md:'justify' }}>
-        Advanced, yet simple. Sophisticated, yet intuitive. 
-      </Text>
+      <Box display="flex" flexDirection="row"  alignItems="center" mt="4" mb="8" >
+          <Text fontSize={{ base: '1.3rem', md:"2rem"}}  textAlign={{ base: 'left', md:'justify' }}>
+            Advanced, yet simple. Sophisticated, yet intuitive &nbsp; 
+            <Image  src={puzzle} width="30" height="30"   />
+          </Text>
+         
+          
+      </Box> 
       <Grid
         templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(3, 1fr)' }}
         columnGap={{ base: '0', lg: '16' }}
