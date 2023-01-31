e8cd1cb31ae7181e9997534df5a522f600a7893e
0xBriz <chaindevone@gmail.com>
2023-01-31 08:22:26 -0500

    formatting

--- components/apr-tooltip/AprTooltip.tsx
+++ components/apr-tooltip/AprTooltip.tsx
@@ -1,115 +1,123 @@
 import { GqlPoolApr } from '~/apollo/generated/graphql-codegen-generated';
-import { Box, Button, Flex, HStack, PlacementWithLogical, Popover, PopoverContent, 
-    PopoverHeader, PopoverTrigger as OrigPopoverTrigger, Text, TextProps } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  PlacementWithLogical,
+  Popover,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger as OrigPopoverTrigger,
+  Text,
+  TextProps,
+} from '@chakra-ui/react';
 import StarsIcon from '~/components/apr-tooltip/StarsIcon';
 import numeral from 'numeral';
 import { AprText } from '~/components/apr-tooltip/AprText';
 import { Info } from 'react-feather';
 
 interface Props {
-    data: GqlPoolApr;
-    textProps?: TextProps;
-    onlySparkles?: boolean;
-    placement?: PlacementWithLogical;
-    aprLabel?: boolean;
-    sparklesSize?: 'sm' | 'md';
+  data: GqlPoolApr;
+  textProps?: TextProps;
+  onlySparkles?: boolean;
+  placement?: PlacementWithLogical;
+  aprLabel?: boolean;
+  sparklesSize?: 'sm' | 'md';
 }
 
 function AprTooltip({ data, textProps, onlySparkles, placement, aprLabel, sparklesSize }: Props) {
-    const PopoverTrigger: React.FC<{ children: React.ReactNode }> = OrigPopoverTrigger;
-    const formatApr = (apr: string) => {
-        if (parseFloat(apr) < 0.0000001) {
-            return '0.00%';
-        }
-        return numeral(apr).format('0.00%');
-    };
+  const PopoverTrigger: React.FC<{ children: React.ReactNode }> = OrigPopoverTrigger;
+  const formatApr = (apr: string) => {
+    if (parseFloat(apr) < 0.0000001) {
+      return '0.00%';
+    }
+    return numeral(apr).format('0.00%');
+  };
 
-return (
-<Popover trigger="hover" placement={placement}>
-    <HStack align="center">
+  return (
+    <Popover trigger="hover" placement={placement}>
+      <HStack align="center">
         {!onlySparkles && (
-            <Text fontSize="1rem" fontWeight="semibold" mr="1" color="white" {...textProps}>
-                {formatApr(data.total)}
-                {aprLabel ? ' APR' : ''}
-            </Text>
+          <Text fontSize="1rem" fontWeight="semibold" mr="1" color="white" {...textProps}>
+            {formatApr(data.total)}
+            {aprLabel ? ' APR' : ''}
+          </Text>
         )}
         <PopoverTrigger>
-            <Button
-                minWidth="0"
-                height="auto"
-                variant="unstyled"
-                _active={{ outline: 'none' }}
-                _focus={{ outline: 'none' }}
-            >
-                {data.hasRewardApr ? (
-                    <StarsIcon
-                        width={sparklesSize === 'sm' ? 18 : 24}
-                        height={sparklesSize === 'sm' ? 19 : 25}
-                    />
-                ) : (
-                    <Box color="gray.200">
-                        <Info size={sparklesSize === 'sm' ? 18 : 24} />
-                    </Box>
-                )}
-            </Button>
+          <Button
+            minWidth="0"
+            height="auto"
+            variant="unstyled"
+            _active={{ outline: 'none' }}
+            _focus={{ outline: 'none' }}
+          >
+            {data.hasRewardApr ? (
+              <StarsIcon
+                width={sparklesSize === 'sm' ? 18 : 24}
+                height={sparklesSize === 'sm' ? 19 : 25}
+              />
+            ) : (
+              <Box color="gray.200">
+                <Info size={sparklesSize === 'sm' ? 18 : 24} />
+              </Box>
+            )}
+          </Button>
         </PopoverTrigger>
-    </HStack>
-    <PopoverContent 
-    w="200px" 
-    padding="4" borderRadius="16px" 
-    bgColor="vertek.slatepurple.900" boxShadow="0 0 12px #000">
+      </HStack>
+      <PopoverContent
+        w="200px"
+        padding="4"
+        borderRadius="16px"
+        bgColor="vertek.slatepurple.900"
+        boxShadow="0 0 12px #000"
+      >
         <PopoverHeader bgColor="vertek.slatepurple.900">
-            <Text textAlign="center" fontSize="1rem">
-                Total APR
-                <br />
-                <span style={{ fontSize: '1.5rem', color:'#4A4AF6' }}>{formatApr(data.total)}</span>
-            </Text>
+          <Text textAlign="center" fontSize="1rem">
+            Total APR
+            <br />
+            <span style={{ fontSize: '1.5rem', color: '#4A4AF6' }}>{formatApr(data.total)}</span>
+          </Text>
         </PopoverHeader>
         <Box p="4" fontSize="md" bgColor="vertek.slatepurple.900">
-            {data.items.map((item, index) => {
-                return (
-                    <Box key={index}>
-                        <Flex>
-                            {formatApr(item.apr)} <AprText>{item.title}</AprText>
-                        </Flex>
-                        {item.subItems?.map((subItem, subItemIndex) => {
-                            const isSubItemsLengthOne = item.subItems?.length === 1;
-                            const isSubItemIndexZero = subItemIndex === 0;
-                            return (
-                                <Flex align="center" key={subItemIndex}>
-                                    <Box
-                                        w="1px"
-                                        m="0.25rem"
-                                        h={
-                                            isSubItemsLengthOne
-                                                ? '0.8rem'
-                                                : isSubItemIndexZero
-                                                ? '1rem'
-                                                : '2rem'
-                                        }
-                                        mt={
-                                            isSubItemsLengthOne
-                                                ? '-0.5rem'
-                                                : isSubItemIndexZero
-                                                ? '-0.3rem'
-                                                : '-1.7rem'
-                                        }
-                                        bgColor="gray.100"
-                                    />
-                                    <Box h="1px" w="0.75rem" mr="0.25rem" ml="-0.25rem" bgColor="gray.100" />
-                                    <Flex>
-                                        {formatApr(subItem.apr)} <AprText>{subItem.title}</AprText>
-                                    </Flex>
-                                </Flex>
-                            );
-                        })}
-                    </Box>
-                );
-            })}
+          {data.items.map((item, index) => {
+            return (
+              <Box key={index}>
+                <Flex>
+                  {formatApr(item.apr)} <AprText>{item.title}</AprText>
+                </Flex>
+                {item.subItems?.map((subItem, subItemIndex) => {
+                  const isSubItemsLengthOne = item.subItems?.length === 1;
+                  const isSubItemIndexZero = subItemIndex === 0;
+                  return (
+                    <Flex align="center" key={subItemIndex}>
+                      <Box
+                        w="1px"
+                        m="0.25rem"
+                        h={isSubItemsLengthOne ? '0.8rem' : isSubItemIndexZero ? '1rem' : '2rem'}
+                        mt={
+                          isSubItemsLengthOne
+                            ? '-0.5rem'
+                            : isSubItemIndexZero
+                            ? '-0.3rem'
+                            : '-1.7rem'
+                        }
+                        bgColor="gray.100"
+                      />
+                      <Box h="1px" w="0.75rem" mr="0.25rem" ml="-0.25rem" bgColor="gray.100" />
+                      <Flex>
+                        {formatApr(subItem.apr)} <AprText>{subItem.title}</AprText>
+                      </Flex>
+                    </Flex>
+                  );
+                })}
+              </Box>
+            );
+          })}
         </Box>
-    </PopoverContent>
-</Popover>
-);
+      </PopoverContent>
+    </Popover>
+  );
 }
 
 export default AprTooltip;
--- components/pool-card/PoolCard.tsx
+++ components/pool-card/PoolCard.tsx
@@ -1,4 +1,15 @@
-import { Box, BoxProps, Flex, LinkBox, HStack, VStack, LinkOverlay, SimpleGrid, Text, GridItem } from '@chakra-ui/react';
+import {
+  Box,
+  BoxProps,
+  Flex,
+  LinkBox,
+  HStack,
+  VStack,
+  LinkOverlay,
+  SimpleGrid,
+  Text,
+  GridItem,
+} from '@chakra-ui/react';
 import AprTooltip from '~/components/apr-tooltip/AprTooltip';
 import Card from '~/components/card/Card';
 import TokenAvatarSet from '~/components/token/TokenAvatarSet';
@@ -14,80 +25,80 @@ export function PoolCard({ pool, ...rest }: Props) {
   const dailyApr = parseFloat(pool.dynamicData.apr.total) / 365;
   const gradient = 'linear-gradient(to right, #4A4AF6, #9B51E0)';
   return (
-
-<LinkBox as="article" flex="1"  {...rest} padding="1" >
-<Card
-    flexDirection="column" 
-    borderRadius="16px"
-    height="327px"
-    padding="4"
-    marginTop="1"
-    boxShadow="0 0 10px #5BC0F8, 0 0 20px #4A4AF6"
-    css={{
-      transition: "transform 0.5s",
-      "&:hover": {
-        transform: "scale(1.02)",
-      },
-    }}
->
-  <Box height="full" bg="vertek.slatepurple.900" borderRadius="md" 
-    boxShadow="2px 24px 12px 0px #000"
-    >
-    <Flex justify="center" padding="2" paddingTop="4" my="4" >
-      <TokenAvatarSet
-        tokenData={pool.allTokens
-        .filter((token) => !token.isNested && !token.isPhantomBpt)
-        .map((token) => ({
-          address: token.address,
-          ...(token.weight && { weight: token.weight }),
-        }))}
-        width={150}
-        imageSize={50}
-        renderPopover={false} />
-    </Flex>
-    <Box  pb="4" mt="3" justifyContent="center">
-        <NextLinkOverlay href={`pool/${pool.id}`}>
-            <Text  
-            fontSize="1.3rem" 
-            color="gray.100"
-            textAlign="center" 
-            fontWeight="bold" 
-            noOfLines={1}>
-              {pool.name}
-            </Text>
-        </NextLinkOverlay>
-    </Box>
-  </Box>
-<Box 
-height="full" 
-marginTop="4" 
-bg="vertek.slatepurple.900" 
-borderRadius="md" 
-boxShadow="2px 24px 12px 0px #000"
->
-    <Box 
-    display="flex"
-    mt="6" 
-    pt="4" 
-    mb="8" 
-    justifyContent="center" 
-    alignItems="center" 
-    flexDirection="column"  >
-        <AprTooltip
+    <LinkBox as="article" flex="1" {...rest} padding="1">
+      <Card
+        flexDirection="column"
+        borderRadius="16px"
+        height="327px"
+        padding="4"
+        marginTop="1"
+        boxShadow="0 0 10px #5BC0F8, 0 0 20px #4A4AF6"
+        css={{
+          transition: 'transform 0.5s',
+          '&:hover': {
+            transform: 'scale(1.02)',
+          },
+        }}
+      >
+        <Box
+          height="full"
+          bg="vertek.slatepurple.900"
+          borderRadius="md"
+          boxShadow="2px 24px 12px 0px #000"
+        >
+          <Flex justify="center" padding="2" paddingTop="4" my="4">
+            <TokenAvatarSet
+              tokenData={pool.allTokens
+                .filter((token) => !token.isNested && !token.isPhantomBpt)
+                .map((token) => ({
+                  address: token.address,
+                  ...(token.weight && { weight: token.weight }),
+                }))}
+              width={150}
+              imageSize={50}
+              renderPopover={false}
+            />
+          </Flex>
+          <Box pb="4" mt="3" justifyContent="center">
+            <NextLinkOverlay href={`pool/${pool.id}`}>
+              <Text
+                fontSize="1.3rem"
+                color="gray.100"
+                textAlign="center"
+                fontWeight="bold"
+                noOfLines={1}
+              >
+                {pool.name}
+              </Text>
+            </NextLinkOverlay>
+          </Box>
+        </Box>
+        <Box
+          height="full"
+          marginTop="4"
+          bg="vertek.slatepurple.900"
+          borderRadius="md"
+          boxShadow="2px 24px 12px 0px #000"
+        >
+          <Box
+            display="flex"
+            mt="6"
+            pt="4"
+            mb="8"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <AprTooltip
               textProps={{ fontSize: '24px', fontWeight: 'normal', mr: '0', lineHeight: '32px' }}
-              data={pool.dynamicData.apr}/>
-        <Text 
-          color="gray.100" 
-          textAlign="center" 
-          fontSize="18px" 
-          lineHeight="24px" >
-            {numeral(dailyApr).format('0.00[0]%')} Daily
-        </Text>
-    </Box>
-</Box>
-    
-  </Card>
-</LinkBox>
-
+              data={pool.dynamicData.apr}
+            />
+            <Text color="gray.100" textAlign="center" fontSize="18px" lineHeight="24px">
+              {numeral(dailyApr).format('0.00[0]%')} Daily
+            </Text>
+          </Box>
+        </Box>
+      </Card>
+    </LinkBox>
   );
 }
