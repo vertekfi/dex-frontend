5cbb8bfad0d1460c22765ea1aed99a085f6cf3df
0xBriz <chaindevone@gmail.com>
2023-02-02 03:57:23 -0500

    remove main pool stake option

--- modules/pool/detail/Pool.tsx
+++ modules/pool/detail/Pool.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, GridItem, VStack, SimpleGrid } from '@chakra-ui/react';
+import { Box, Text, GridItem, VStack, SimpleGrid, Button } from '@chakra-ui/react';
 import PoolHeader from '~/modules/pool/detail/components/PoolHeader';
 import { PoolComposition } from '~/modules/pool/detail/components/composition/PoolComposition';
 import PoolStats from './components/stats/PoolStats';
@@ -12,11 +12,13 @@ import { PoolStatistics } from '~/modules/pool/detail/components/PoolStatistics'
 import { PoolStakeModal } from '~/modules/pool/stake/PoolStakeModal';
 import { PoolUnstakeModal } from '~/modules/pool/stake/PoolUnstakeModal';
 import numeral from 'numeral';
+import { networkConfig } from '~/lib/config/network-config';
 
 export function Pool() {
   const { pool } = usePool();
   const depositFee = pool.staking?.gauge?.depositFee || 0;
   const withdrawalFee = pool.staking?.gauge?.withdrawFee || 0;
+  const isMainPool = networkConfig.balancer.votingEscrow.lockablePoolId === pool.id;
 
   return (
     <Box marginBottom="8">
@@ -38,46 +40,95 @@ export function Pool() {
             <PoolInvestModal />
             <PoolWithdrawModal />
           </Box>
-          <Box
-            width={{ base: 'auto', md: '25%' }}
-            display={{ base: 'flex', md: 'block' }}
-            flexDirection={{ base: 'column', md: 'row' }}
-          >
-            <Text
-              fontWeight="bold"
-              fontSize="1.1rem"
-              mb="1"
-              textAlign={{ base: 'right', md: 'center' }}
-              mr={{ base: 'auto', md: '0' }}
-              color="gray.100"
+
+          {isMainPool ? (
+            <GridItem
+              className="verteklightpurplebox"
+              //  marginTop={{ base: 0, lg: 2 }}
+              display="flex"
+              // width={{ base: '100%', md: '60%', lg: 'auto' }}
+              //    mx="auto"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+              // colSpan={{ sm: 1, lg: 2 }}
+              flexDirection="column"
+              borderRadius={{ base: '16px', lg: '16px' }}
+              padding={{ base: '4', lg: '4' }}
             >
-              Stake VPT Tokens
-            </Text>
-            <PoolStakeModal />
-            <PoolUnstakeModal />
-            <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
               <Text
-                fontSize="0.8rem"
-                textAlign={{ base: 'right', md: 'left' }}
-                ml={{ base: 'auto', md: '0' }}
-                width="50%"
-                color="gray.100"
+                fontSize={{ base: '1rem', lg: '1.1rem' }}
+                color="white"
+                fontWeight="semibold"
+                m="2"
+                textAlign="left"
               >
-                Deposit fee:&nbsp;
-                {numeral(depositFee / 10000).format('0.0%')}
+                Earn more VRTK in Core Pools
               </Text>
               <Text
-                fontSize="0.8rem"
-                textAlign={{ base: 'right', md: 'left' }}
-                mr={{ base: 'auto', md: '3' }}
-                width="50%"
+                fontSize={{ base: '1rem', lg: '1.0rem' }}
+                fontWeight="normal"
+                m="2"
+                color="vertek.slate.100"
+                textAlign="left"
+              >
+                Lock VRTK-BNB and receive up to 2.5x boosted rewards when farming Core Pools.
+              </Text>
+              <Button
+                variant="moistblack"
+                marginBottom="1rem"
+                width={{ base: '75%', lg: '75%' }}
+                boxShadow="0px 2px 2px 4px #000"
+                fontSize={{ base: '0.9rem', lg: '1rem' }}
+                mt="3"
+                as="a"
+                alignSelf="center"
+                href={'/voting'}
+              >
+                Earn More
+              </Button>
+            </GridItem>
+          ) : (
+            <Box
+              width={{ base: 'auto', md: '25%' }}
+              display={{ base: 'flex', md: 'block' }}
+              flexDirection={{ base: 'column', md: 'row' }}
+            >
+              <Text
+                fontWeight="bold"
+                fontSize="1.1rem"
+                mb="1"
+                textAlign={{ base: 'right', md: 'center' }}
+                mr={{ base: 'auto', md: '0' }}
                 color="gray.100"
               >
-                Withdrawal fee:&nbsp;
-                {numeral(withdrawalFee / 10000).format('0.0%')}
+                Stake VPT Tokens
               </Text>
+              <PoolStakeModal />
+              <PoolUnstakeModal />
+              <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
+                <Text
+                  fontSize="0.8rem"
+                  textAlign={{ base: 'right', md: 'left' }}
+                  ml={{ base: 'auto', md: '0' }}
+                  width="50%"
+                  color="gray.100"
+                >
+                  Deposit fee:&nbsp;
+                  {numeral(depositFee / 10000).format('0.0%')}
+                </Text>
+                <Text
+                  fontSize="0.8rem"
+                  textAlign={{ base: 'right', md: 'left' }}
+                  mr={{ base: 'auto', md: '3' }}
+                  width="50%"
+                  color="gray.100"
+                >
+                  Withdrawal fee:&nbsp;
+                  {numeral(withdrawalFee / 10000).format('0.0%')}
+                </Text>
+              </Box>
             </Box>
-          </Box>
+          )}
         </Box>
       </Box>
       <SimpleGrid
