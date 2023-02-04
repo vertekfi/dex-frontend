c7117caf5f8f1b7258346162f1ecef30121dc94a
0xBriz <chaindevone@gmail.com>
2023-02-04 08:16:35 -0500

    swap merge

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApolloClient, loadApolloState } from '~/apollo/client';
 import Head from 'next/head';
 import { TradeContainer } from '~/modules/trade/TradeContainer';
@@ -8,6 +7,7 @@ import SwapMastheadImage from '~/assets/images/swap-masthead-image.png';
 import SwapMastheadOpImage from '~/assets/images/swap-masthead-image-OP.png';
 import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
 import { UserTokenBalancesProvider } from '~/lib/user/useUserTokenBalances';
+import { Text, Box } from '@chakra-ui/react';
 import 'animate.css';
 
 function Swap() {
@@ -56,63 +56,3 @@ export async function getStaticProps() {
 }
 
 export default Swap;
-=======
-import { initializeApolloClient, loadApolloState } from '~/apollo/client';
-import Head from 'next/head';
-import { TradeContainer } from '~/modules/trade/TradeContainer';
-import { PageMasthead } from '~/components/masthead/PageMasthead';
-import NextImage from 'next/image';
-import SwapMastheadImage from '~/assets/images/swap-masthead-image.png';
-import SwapMastheadOpImage from '~/assets/images/swap-masthead-image-OP.png';
-import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
-import { UserTokenBalancesProvider } from '~/lib/user/useUserTokenBalances';
-import { Text, Box } from '@chakra-ui/react';
-import 'animate.css'; 
-
-function Swap() {
-    const { chainId } = useNetworkConfig();
-
-    const TITLE = 'Vertek | Swap';
-    const DESCRIPTION = 'Intelligent trades at optimal prices. Swap your tokens with the Smart Order Router.';
-
-    return (
-        <>
-            <Head>
-                <title>{TITLE}</title>
-                <meta name="title" content={TITLE} />
-                <meta property="og:title" content={TITLE} />
-                <meta property="twitter:title" content={TITLE} />
-
-                <meta name="description" content={DESCRIPTION} />
-                <meta property="og:description" content={DESCRIPTION} />
-                <meta property="twitter:description" content={DESCRIPTION} />
-            </Head>
-            <UserTokenBalancesProvider>
-                <PageMasthead
-                    title="Swap"
-                    image={
-                        <NextImage
-                            src={chainId === '10' ? SwapMastheadOpImage : SwapMastheadImage}
-                            width="213.71px"
-                            height="68px"
-                        />
-                       
-                    }
-                />
-                {/* <Box width={{ base:'100%', lg:'60%' }}>
-                <Text fontSize="1.2rem" mt="-4" mb="6"> Utilize Vertek's decentralized exchange to swap tokens with lightning speed and maximal efficiency </Text>
-                </Box> */}
-                <TradeContainer />
-            </UserTokenBalancesProvider>
-        </>
-    );
-}
-
-export async function getStaticProps() {
-    const client = initializeApolloClient();
-
-    return loadApolloState({ client });
-}
-
-export default Swap;
->>>>>>> fb23a6b (animations merge)
